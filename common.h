275681e7c37f315fb27c9640d164b8d6d0cd53e7
limhuiseong <limhuiseong.cs@gmail.com>
2025-01-25 07:31:32 +0000

    Implement printf using putchar

--- common.h
+++ common.h
@@ -1,7 +1,13 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#define va_list  __builtin_va_list
+#define va_start __builtin_va_start
+#define va_end   __builtin_va_end
+#define va_arg   __builtin_va_arg
+
 void *memset(void *buf, char c, size_t n);
 void putchar(char ch);
+void printf(const char *fmt, ...);
 
 #endif
