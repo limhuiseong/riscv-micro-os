03f8a1536baf9b666f2c521b21731d8796140108
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-01-28 10:53:09 +0000

    Implement strcmp function and add its declaration

--- common.c
+++ common.c
@@ -86,3 +86,14 @@ char *strcpy(char *dst, const char *src) {
     *d = '\0';
     return dst;
 }
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
