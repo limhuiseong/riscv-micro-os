e32ad8006cb82410104eeb791eabe7c224f5c0bf
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-01-28 10:40:16 +0000

    Add clang built-in type definitions and macros

--- common.h
+++ common.h
@@ -6,9 +6,21 @@
 #define va_end   __builtin_va_end
 #define va_arg   __builtin_va_arg
 
+#define true  1
+#define false 0
+#define NULL  ((void *) 0)
+
+#define align_up(value, align)   __builtin_align_up(value, align)
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+#define offsetof(type, member)   __builtin_offsetof(type, member)
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
+typedef int bool;
+typedef uint32_t size_t;
+typedef uint32_t paddr_t;
+typedef uint32_t vaddr_t;
 
 void *memset(void *buf, char c, size_t n);
 void putchar(char ch);
