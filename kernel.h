51546a34eba1f6d352459c0b7c3ab1838a0b1698
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-01-29 14:24:13 +0000

    Modify sbiret to align with riscv-sbi documentation

--- kernel.h
+++ kernel.h
@@ -64,7 +64,10 @@ struct trap_frame
 struct sbiret
 {
     long error;
-    long value;
+    union {
+        long value;
+        unsigned long uvalue;
+    };
 };
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid);
