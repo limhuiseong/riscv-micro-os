9e1b56cccc5c9f644fea5fc9109924113593bc10
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-01-30 13:39:24 +0000

    Implement and test context switching

--- kernel.h
+++ kernel.h
@@ -26,6 +26,10 @@
         }                                                                                                              \
     } while (0)
 
+#define PROCS_MAX     8
+#define PROC_UNUSED   0
+#define PROC_RUNNABLE 1
+
 struct trap_frame
 {
     uint32_t ra;
@@ -72,6 +76,13 @@ struct sbiret
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid);
 
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
+
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void);
 
 void kernel_main(void);
@@ -81,5 +92,11 @@ __attribute__((naked)) __attribute__((aligned(4))) void kernel_entry(void);
 void handle_trap(struct trap_frame *f);
 
 paddr_t alloc_pages(uint32_t n);
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp);
+
+struct process *create_process(uint32_t pc);
+
+void delay(void);
 
 #endif
