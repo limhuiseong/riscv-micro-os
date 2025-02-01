606a3221b742db1ed67f1ef0e7dfaee7392d7b2c
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-02-01 13:35:59 +0000

    Fix bash syntax

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     boot/boot.s \
     init/main.c \
-    kernel/console.c kernel/delay.c kernel/sched.c kernel/string.c kernel/trap.c \
-    mm/page_alloc.c mm/page_table.c \
-    riscv/sbi.c
+    kernel/console.c kernel/delay.c kernel/sched.c kernel/string.c kernel/trap.c kernel/arch/riscv/sbi.c \
+    mm/page_alloc.c mm/page_table.c
+
 QEMU=qemu-system-riscv32
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
