b969c5c5384b60def9bb9ad5f0077e9bb56cf939
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-02-01 08:53:45 +0000

    Rename page_alloc.h to mm.h for clarity

---
+++ include/mm.h
@@ -0,0 +1,21 @@
+#ifndef MM_H
+#define MM_H
+
+#include "types.h"
+
+#define align_up(value, align) __builtin_align_up(value, align)
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+#define offsetof(type, member) __builtin_offsetof(type, member)
+
+#define PAGE_SIZE 4096
+
+#define SATP_SV32 (1u << 31)
+#define PAGE_V (1 << 0)
+#define PAGE_R (1 << 1)
+#define PAGE_W (1 << 2)
+#define PAGE_X (1 << 3)
+#define PAGE_U (1 << 4)
+
+paddr_t alloc_pages(uint32_t n);
+
+#endif
